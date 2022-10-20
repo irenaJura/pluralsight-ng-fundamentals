f134a3b88bad23314f9f67706178106b48a198be
Irena Jurasek <irenaskydive@gmail.com>
2022-10-20 12:09:55 +0200

    creating components and communication between them

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { EventsAppComponent } from './events-app.component';
+import { EventListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    EventsAppComponent,
+    EventListComponent,
+    EventThumbnailComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
---
+++ src/app/events/event-thumbnail.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+
+@Component({
+    selector: 'event-thumbnail',
+    template: `
+    <div class="well hoverwell thumbnail">
+        <h2>{{event.name}}</h2>
+        <div>Date: {{event.date}}</div>
+        <div>Time: {{event.time}}</div>
+        <div>Price: $\{{event.price}}</div>
+        <div>
+            <span>Location: {{event.location.address}}</span>
+            <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+        </div>
+    </div>
+    <button class="btn btn-primary" (click)="handleClickMe()">
+    Click me!</button>
+    `,
+    styles: [`
+    .pad-left { margin-left: 10px; }
+    .well div { color: #bbb; }
+    `]
+})
+
+export class EventThumbnailComponent {
+    @Input() event: any; // [event] in parent template
+    @Output() eventClick = new EventEmitter(); // (eventClick)
+    someProperty: any = "some value";
+
+    handleClickMe() {
+        this.eventClick.emit(this.event.name);
+    }
+
+    logFoo() {
+        console.log("foo")
+    }
+}
