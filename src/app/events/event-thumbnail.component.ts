c9d515b0acf4c828638cccf2ce915aa8dec07ff4
Irena Jurasek <irenaskydive@gmail.com>
2022-10-20 15:09:44 +0200

    template syntax

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -4,19 +4,28 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
     selector: 'event-thumbnail',
     template: `
     <div class="well hoverwell thumbnail">
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: $\{{event.price}}</div>
-        <div>
-            <span>Location: {{event.location.address}}</span>
-            <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+        <h2>{{event?.name}}</h2>
+        <div>Date: {{event?.date}}</div>
+        <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
+            Time: {{event?.time}}
+            <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+            <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+            <span *ngSwitchDefault>(Normal Start)</span>
+        </div>
+        <div>Price: $\{{event?.price}}</div>
+        <div *ngIf="event?.location">
+            <span>Location: {{event?.location?.address}}</span>
+            <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+        </div>
+        <div *ngIf="event?.onlineUrl">
+            Online URL: {{event?.onlineUrl}}
         </div>
     </div>
-    <button class="btn btn-primary" (click)="handleClickMe()">
-    Click me!</button>
     `,
     styles: [`
+    .green { color: #003300 !important; }
+    .bold { font-weight: bold; }
+    .thumbnail { min-height: 210px; }
     .pad-left { margin-left: 10px; }
     .well div { color: #bbb; }
     `]
@@ -24,14 +33,10 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
 
 export class EventThumbnailComponent {
     @Input() event: any; // [event] in parent template
-    @Output() eventClick = new EventEmitter(); // (eventClick)
-    someProperty: any = "some value";
 
-    handleClickMe() {
-        this.eventClick.emit(this.event.name);
-    }
-
-    logFoo() {
-        console.log("foo")
+    getStartTimeClass() {
+        if (this.event && this.event.time === '8:00 am')
+            return ['green', 'bold'];
+        return [];
     }
 }
